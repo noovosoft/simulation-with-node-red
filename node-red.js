345cda6209668a2506b7470b6da125bbf1480b6d
shreyash-noovosoft <shreyash.deshpande@noovosoft.com>
2023-08-31 19:13:56 +0530

    modification/updates in firebase data fetching process

--- node-red.js
+++ node-red.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
-import {getDatabase, ref, get, onValue, child} from "https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js";
+import {getDatabase, ref, query, get, onValue, onChildAdded, onChildChanged, limitToLast, child} from "https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -36,21 +36,20 @@ function convertAverage(arr, num) {
     }
     return avgArr;
 }
-const voltageRef = ref(db, 'node-db/voltage');
+const voltageRef = query(ref(db, 'node-db/voltage'), limitToLast(1080));
 onValue(voltageRef, (snapshot) => {
     let voltage = []
     const data = snapshot.val();
     for(let timeline in data) {
-        //console.log(key + " " + data[key])
         voltage.push(data[timeline])
     }
 
     const averageVoltage = convertAverage(voltage, 18) //input per 10sec = 18 input/3mins
-    temperatureVoltage.plotPoints(voltage.slice(60), "red")
+    temperatureVoltage.plotPoints(voltage.reverse().slice(-60), "red")
     voltageObj.plotPoints(averageVoltage, "red")
 });
 
-const temperatureRef = ref(db, 'node-db/temperature');
+const temperatureRef = query(ref(db, 'node-db/temperature'), limitToLast(1080));
 onValue(temperatureRef, (snapshot) => {
     const data = snapshot.val();
     let temperature = []
@@ -58,7 +57,6 @@ onValue(temperatureRef, (snapshot) => {
         temperature.push(data[timeline]);
     }
     const averageTemperature = convertAverage(temperature, 18)
-    temperatureVoltage.plotPoints(temperature.slice(60));
+    temperatureVoltage.plotPoints(temperature.reverse().slice(-60));
     temperatureObj.plotPoints(averageTemperature);
 });
-
